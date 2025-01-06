dc51f8f7ca291d548c910b8ed5d77c4dee697c2b
Devraj Kashyap <96662811+dev-rajk@users.noreply.github.com>
2025-01-06 15:25:15 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from datetime import datetime
-from db import get_events, add_event
+from db import add_event, get_events_dated
 import pandas as pd
 st.set_page_config(
     page_title="Assam Quiz Calendar",
@@ -14,24 +14,27 @@ st.set_page_config(
     )
 # Title of the Streamlit app
 st.title("Assam Quiz Calendar :calendar: ")
-st.write('Select in sidebar to view quizzes or submit events')
+
+st.write("Find upcoming events or add your own events")
 
 # Fetch approved events (assuming get_events fetches a list of events)
-approved_events = get_events("Approved")
+upcoming_events = get_events_dated(str(datetime.now().date()))
 # Convert the list of events into a DataFrame (assuming approved_events is a list of dictionaries)
-df = pd.DataFrame(approved_events, columns= ['quiz_name', 'date', 'time', 'category', 'venue', 'location', 'organizer', 'genre', 'quiz_master', 'prize', 'contact_number', 'registration_link', 'other_details' ])
+df = pd.DataFrame(upcoming_events, columns= ['quiz_name', 'date', 'time', 'category', 'venue', 'location', 'organizer', 'genre', 'quiz_master', 'prize', 'contact_number', 'registration_link', 'other_details' ])
     
-# Check if the DataFrame has the necessary columns: 'event_name' and 'event_date' (adjust as needed)
+
 
 # Convert event_date from string to datetime (in the format 'YYYY-MM-DD')
 df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d', errors='coerce')
 df['date'] = df['date'].dt.date
 
-# Filter the DataFrame to include only future events
-df = df[df['date'] > datetime.now().date()]
 
 # Sort the events by the 'date'
 df = df.sort_values(by='date')
+
+# Format the 'date' column to the desired format: "12 May, 2025"
+df['date'] = df['date'].apply(lambda x: x.strftime('%d %b, %Y'))
+
 
 # Reset index to remove the index column when displaying the table
 df_reset = df[['quiz_name', 'date', 'time', 'category', 'venue', 'location', 'prize', 'contact_number', 'organizer', 'genre', 'quiz_master', 'registration_link', 'other_details']].reset_index(drop=True)
@@ -57,7 +60,7 @@ df_reset = df_reset.rename(columns={
 # Display the table with clickable links
 st.markdown(df_reset.to_html(escape=False), unsafe_allow_html=True)
 
-st.subheader("Submit a New Quiz Event")
+st.subheader("Submit an Upcoming Quiz ")
 # clearornot = st.checkbox(label="clear on exit")
 with st.form(key='event_form', clear_on_submit=True):
         st.write(''':red[Please ensure that event details are accurate] ''') 
@@ -85,8 +88,8 @@ with st.form(key='event_form', clear_on_submit=True):
                 add_event(Title, Date, Time, Category, Venue, Location, Organizer, Genre, Quiz_Master, Prize, Contact, registration_link, other_details)
                 st.success(f"Event '{Title}' added!!!")
 st.write('''
+                :red[**DISCLAIMER**] \n
                 :red[**This website is free of cost for all users.** ] \n
-                :red[**The maintainers bear no responsibility for the accuracy of the event information, or any loss or damages resulting from the use of this site. The maintainer is not liable for any damages or misinformation.**] \n
                 :red[**Users are encouraged to verify all event information.**] \n
                 :red[**No personal infomation is tracked.**] \n
                 :green[**Cheers to quizzing.** ] :partying_face::partying_face:\n
